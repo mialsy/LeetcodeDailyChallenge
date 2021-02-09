2746a2a969e7b98d53067b13682c63ae0d8a4b4e
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-08 23:37:23 -0800

    add LC41 solution

---
+++ src/Other/LC41_FirstMissingNumber.java
@@ -0,0 +1,24 @@
+package Other;
+
+public class LC41_FirstMissingNumber {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != i + 1) {
+                // remember to avoid the infinate loop
+                if (nums[i] == nums[nums[i] - 1]) break;
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
