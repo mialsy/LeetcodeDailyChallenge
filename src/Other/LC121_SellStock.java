c9b2b32d0e179a1995604d026fdc601f0391bf1d
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-08 23:35:55 -0800

    add solutio to lc121

---
+++ src/Other/LC121_SellStock.java
@@ -0,0 +1,18 @@
+package Other;
+
+public class LC121_SellStock {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int current = prices[i];
+
+            if (min > current) {
+                min = current;
+            } else {
+                maxProfit = Math.max(maxProfit, current - min);
+            }
+        }
+        return maxProfit;
+    }
+}
